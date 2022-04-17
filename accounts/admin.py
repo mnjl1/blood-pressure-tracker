289bcc1c67c8556b7de82dcf71cfcc4eb830d84f
mnjl1 <d.manzhula@gmail.com>
2022-04-18 00:14:12 +0300

    allauth email and password registration

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,28 +1,17 @@
 from pyexpat import model
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
 
+
+CustomUser = get_user_model()
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active')
-    list_filter = ('email', 'is_staff', 'is_active')
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')
-        }),
-    )
-    search_fiels = ('email', )
-    ordering = ('email', )
+    list_display = ('email', 'username', 'is_staff', 'is_active')
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/tests.py
+++ accounts/tests.py
@@ -5,33 +5,17 @@ class UserManagersTests(TestCase):
 
     def test_create_uses(self):
         User = get_user_model()
-        user = User.objects.create_user(email='user@email.com', password='testpass')
+        user = User.objects.create_user(email='user@email.com', username='vasia', password='testpass')
         self.assertEqual(user.email, 'user@email.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
-        try:
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email='')
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="testpass")
+        
 
     def test_create_superuser(self):
         User = get_user_model()
-        admin_user = User.objects.create_superuser(email='admin@email.com', password='testpass')
+        admin_user = User.objects.create_superuser(email='admin@email.com', username='vasia', password='testpass')
         self.assertEqual(admin_user.email, 'admin@email.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
-        try:
-            self.assertIsNone(admin_user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(ValueError):
-            User.objects.create_superuser(
-                email='admin@email.com', password='testpass', is_superuser=False)
